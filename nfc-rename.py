22a5990535fb9b685368fe3fe5de43da8aac2dba
woodie2wopper <torir.lab@gmail.com>
2024-04-04 15:11:17 +0900

    リネームファイルのリストまでできた

--- nfc-rename.py
+++ nfc-rename.py
@@ -41,15 +41,9 @@ def convert_epoch_to_666(start_epoch, stop_epoch):
     stop_x6  = convert_epoch_to_x6(stop_epoch)
     return start_66 + stop_x6
 
-def get_666(filename,mtime,duration,isStart):
-    if isStart:
-        start_epoch = mtime
-        stop_epoch = mtime + duration
-    else:
-        start_epoch = mtime - duration
-        stop_epoch = mtime
+def get_666(filename,start_epoch,stop_epoch,name_site):
     body_666 = convert_epoch_to_666(start_epoch,stop_epoch)
-    new_filename = f'{body_666}_{selected_site}_{filename}'
+    new_filename = f'{body_666}_{name_site}_{filename}'
     return new_filename
 
     
@@ -69,74 +63,75 @@ def get_metadata_sounds(list_sounds,directory):
 
 # 同一の録音をmtimeでグループ化する。中身はmetadata
 def grouping_sounds(metadata_sounds):
-    file_groups = {}  # 辞書として初期化
+    file_groups = {}
     # 連続するファイルでmtimeが同じなら同一録音と判断する
     for metadata in sorted(metadata_sounds, key=lambda x: x['filename'], reverse=True):
         mtime = int(metadata['mtime'])
         if mtime not in file_groups:
             file_groups[mtime] = []  # 新しいキーに対して空のリストを作成
         file_groups[mtime].append(metadata)
+    #for mtime, group in file_groups.items():
     return file_groups
 
 
+def is_set_ICR():
+    try:
+        isStart = dict_ICR[selected_ICR] == 'START'
+        return True
+    except KeyError:
+        return False
+
+
+def is_set_site():
+    return bool(selected_site)
+
+# dlg = ft.AlertDialog(
+#     title=ft.Text("Hello, you!"), on_dismiss=lambda e: print("Dialog dismissed!")
+# )
+# 
+
+# def close_dlg(e):
+#     dlg_modal.open = False
+#     page.update()
+# 
+
+
+# def open_dlg_modal(e):
+#     global selected_site
+#     # dlg_modalのcontentを動的に生成して更新
+#     dlg_modal.content = ft.Column([
+#         ft.Text("サイト名: " + dict_site['サイト名']),
+#         #ft.Text("緯度: " + dict_site['緯度']),
+#         #ft.Text("経度: " + dict_site['経度']),
+#         #ft.Text("県名: " + dict_site['県名']),
+#         #ft.Text("市町村: " + dict_site['市町村'])
+#     ])
+#     selected_site = dict_site['サイト名']
+#     page.dialog = dlg_modal
+#     dlg_modal.open = True
+#     page.update()
+#     update_site_info()
+
+
+# dlg_modal = ft.AlertDialog(
+#     modal=True,
+#     title=ft.Text("サイト情報の確認"),
+#     content=ft.Column([]),
+#     actions=[
+#         ft.TextButton("ok", on_click=close_dlg),
+#     ],
+#     actions_alignment=ft.MainAxisAlignment.END,
+#     on_dismiss=lambda e: print("Modal dialog dismissed!"),
+# )
+
+# def open_dlg(e):
+#     page.dialog = dlg
+#     dlg.open = True
+#     page.update()
+
+
+
 def main(page: ft.Page):
-
-
-    dlg = ft.AlertDialog(
-        title=ft.Text("Hello, you!"), on_dismiss=lambda e: print("Dialog dismissed!")
-    )
-    
-
-    def close_dlg(e):
-        dlg_modal.open = False
-        page.update()
-    
-
-
-    def open_dlg_modal(e):
-        global selected_site
-        # dlg_modalのcontentを動的に生成して更新
-        dlg_modal.content = ft.Column([
-            ft.Text("サイト名: " + dict_site['サイト名']),
-            #ft.Text("緯度: " + dict_site['緯度']),
-            #ft.Text("経度: " + dict_site['経度']),
-            #ft.Text("県名: " + dict_site['県名']),
-            #ft.Text("市町村: " + dict_site['市町村'])
-        ])
-        selected_site = dict_site['サイト名']
-        page.dialog = dlg_modal
-        dlg_modal.open = True
-        page.update()
-        update_site_info()
-
-
-
-    def set_name_site(e=None):
-        #global selected_site
-        #selected_site = dict_site['サイト名']
-        #page.dialog = dlg_modal
-        #dlg_modal.open = True
-        #page.update()
-        update_site_info()
-        name_site.value = selected_site
-        name_site.update()
-
-
-    dlg_modal = ft.AlertDialog(
-        modal=True,
-        title=ft.Text("サイト情報の確認"),
-        content=ft.Column([]),
-        actions=[
-            ft.TextButton("ok", on_click=close_dlg),
-        ],
-        actions_alignment=ft.MainAxisAlignment.END,
-        on_dismiss=lambda e: print("Modal dialog dismissed!"),
-    )
-
-    def open_dlg(e):
-        page.dialog = dlg
-        dlg.open = True
-        page.update()
 
 
     def update_dict_site(key, value):
@@ -149,7 +144,6 @@ def main(page: ft.Page):
     def on_dropdown_change(e):
         global selected_ICR  # selected_ICRをグローバル変数として宣言
         selected_ICR = e.control.value  # 選択されたOptionのvalue属性がキーになります
-        # print(f"選択されたICレコーダのキー: {selected_ICR}")
         update_ICR_info()
 
 
@@ -173,141 +167,103 @@ def main(page: ft.Page):
         output_dir_path.update()
 
 
-    # 指定されたパスから音声ファイルのリストを取得し、情報を更新する
-    def update_sounds_list_and_rename_list(e: ft.FilePickerResultEvent):
-        #msg = ""
-        list_sounds = []
-        if e.path:
-            info_sound_dir.value = e.path
-            #list_sounds = get_merge_sounds_group(e.path)
-            list_sounds = get_sounds_list(e.path)
-            update_sounds_info(list_sounds,e.path)
-            update_rename_info(list_sounds)
+    # ファイル名の変更に関する情報を更新する関数
+    def update_rename_info(file_groups):
+        msg=""
+        isStart = None
+        
+        def get_renamed_sounds(files_in_same_groups):
+            mtime = files[0]['mtime']
+            total_duration = sum(file['duration'] for file in files_in_same_groups)
+            start_epoch, stop_epoch = (mtime, mtime + total_duration) if isStart else (mtime - total_duration, mtime)
+            new_filename = get_666(filename, start_epoch, stop_epoch, selected_site)
+
+            return new_filename
+
+        if is_set_site() & is_set_ICR() :
+            isStart = dict_ICR[selected_ICR] == 'START'
+            for mtime, files in file_groups.items():
+                if len(files) == 1:
+                    # 単一ファイルの場合、リストの最初の要素の 'filename' キーを使用
+                    filename = files[0]['filename']
+                else:
+                    # 拡張子を除いたファイル名を連結し、最後に拡張子を追加する
+                    filename = '_'.join(f['filename'].rsplit('.', 1)[0] for f in files) + '.' + files[0]['filename'].split('.')[-1]
+                new_filename = get_renamed_sounds(files)
+                msg += f"{filename} -> {new_filename}\n"
         else:
-            info_sound_dir.value = "Cancelled!"
-        info_sound_dir.update()
+            msg = f"Error: 'ICレコーダもしくはサイト名が設定されていません'"
+                
+        renamed_info.value = msg
+        renamed_info.update()
+    
 
-
-    def update_ICR_info():
-        info_selected_ICR.value = selected_ICR
+    def update_ICR_info(e=None):
+        name_ICR.value = f'ICレコーダー[{selected_ICR}] タイムスタンプ：{dict_ICR[selected_ICR]}'
+        name_ICR.update()
+        info_selected_ICR.value = f'{selected_ICR}: {dict_ICR[selected_ICR]}'
         info_selected_ICR.update()
 
 
-    def update_site_info():
+    def update_site_info(e=None):
+        name_site.value = f'ファイル名に追加されるサイト名：{selected_site}'
+        name_site.update()
         info_selected_site.value = selected_site
         info_selected_site.update()
 
-    def update_sounds_info(list_sounds, directory):
+
+    def update_sounds_info(file_groups):
         msg = ""
-        file_groups = {}
-        # サウンドファイルのtimestampのリスト作成
-        metadata_sounds = get_metadata_sounds(list_sounds,directory)
-        file_groups = grouping_sounds(metadata_sounds)
+        str_group = ""
         counter_group = 0
         for mtime, files in file_groups.items():
+            print(mtime)
             if len(files) == 1:
-                flag_group = ""
+                str_group = ""
             else:
                 counter_group +=1
-                flag_group = f"同一音源[{counter_group}]: "
-            # グループ内にファイルが1つだけの場合
-            # 選択されたfiles[0]のfilename, mtime, durationを取得し、isStartを設定する
+                str_group = f"同一音源-{counter_group}: "
+            # 選択されたfiles[0]のfilename, mtime, durationを取得
             for selected_file in files:
                 filename = selected_file['filename']
                 mtime = selected_file['mtime']
                 duration = selected_file['duration']
                 mtime_formatted = convert_epoch_to_string(mtime)
                 duration_formatted = str(timedelta(seconds=duration))
-                msg += f"{flag_group}{filename}: 変更時刻: {mtime_formatted}, 長さ: {duration_formatted}\n"
-#            selected_file = files[0]
-#            filename = selected_file['filename']
-#            mtime = selected_file['mtime']
-#            duration = selected_file['duration']
-#            #isStart = dict_ICR[selected_ICR] == 'START'
-#            mtime_formatted = convert_epoch_to_string(mtime)
-#            duration_formatted = str(timedelta(seconds=duration))
-#            #new_filename = get_666(filename=filename, mtime=mtime, duration=duration, isStart=isStart)
-#            msg += f"{flag_group}:{filename}: 変更時刻: {mtime_formatted}, 長さ: {duration_formatted}\n"
-#                #new_filename = files
-                    
-        #    prev_mtime = 0
-        #    prev_start_epoch = 0
-        #    # 連続するファイルでmtimeが同じなら同一録音と判断する
-        #    for timestamp in sorted(metadata_sounds, key=lambda x: x['filname'], reverse=True):
-        #        isGroup = False
-        #        mtime_epoch = timestamp['mtime']
-        #        mtime_formatted = convert_epoch_to_string(mtime_epoch)
-        #        #print(f"mtime_epoch: {mtime_epoch}, ファイル名: {timestamp['filname']}")
-        #        if mtime_epoch not in file_groups:
-        #            file_groups[mtime_epoch] = []
-        #            # 同一録音グループの条件
-        #            if mtime_epoch == prev_mtime:
-        #                stop_epoch = prev_start_epoch
-        #                isGroup = True
-        #            else:
-        #                stop_epoch = timestamp['mtime']
-        #        file_groups[mtime_epoch].append(timestamp['filname'])
+                msg += f"{str_group}{filename}: 変更時刻: {mtime_formatted}, 長さ: {duration_formatted}\n"
 
-        #        duration = timestamp['duration']
-        #        start_epoch = stop_epoch - duration
-        #        duration_formatted = str(timedelta(seconds=timestamp['duration']))
-        #        start_formatted = convert_epoch_to_string(start_epoch)
-        #        stop_formatted = convert_epoch_to_string(stop_epoch)
-        #        if isGroup:
-        #            msg += f" G:{timestamp['filname']}: 変更時刻: {mtime_formatted}, 長さ: {duration_formatted}\n"
-        #        else:
-        #            msg += f" {timestamp['filname']}: 変更時刻: {mtime_formatted}, 長さ: {duration_formatted}\n"
+        sound_info.value = msg
+        sound_info.update()
+        
 
-        #        # 一つ前を記憶する
-        #        prev_mtime = mtime_epoch
-        #        prev_start_epoch = start_epoch
-        #return msg
-
-        def set_grouped_sound(file_groups):
-            # 同じmtimeを持つファイルをリスト化して返す
-            def group_files_by_mtime(file_groups):
-                grouped_files = {}
-                for mtime, files in file_groups.items():
-                    if mtime not in grouped_files:
-                        grouped_files[mtime] = []
-                    # grouped_files[mtime]リストにfilesリストの要素を追加している
-                    grouped_files[mtime].extend(files)
-                return grouped_files
-
-            grouped_files = group_files_by_mtime(file_groups)
-            return grouped_files
-
-        #msg = set_metadata_sounds_group(metadata_sounds)
-        sounds_info_in_directory.value = msg
-        sounds_info_in_directory.update()
-        grouped_sound = set_grouped_sound(file_groups)
-        print(grouped_sound)
+    # 指定されたパスから音声ファイルのリストを取得し、情報欄を更新する
+    def update_sounds_list_and_rename_list(e: ft.FilePickerResultEvent):
+        list_sounds = []
+        if e.path:
+            info_sound_dir.value = e.path
+            list_sounds = get_sounds_list(e.path)
+            metadata_sounds = get_metadata_sounds(list_sounds,e.path)
+            file_groups = grouping_sounds(metadata_sounds)
+            update_sounds_info(file_groups) # sound欄の更新
+            update_rename_info(file_groups) # rename欄の更新
+        else:
+            info_sound_dir.value = "Cancelled!"
+        info_sound_dir.update()
 
 
-
-    def update_rename_info(list_sounds):
-        msg=""
-        for file in list_sounds:
-            print(file)  # ここでfile変数を使用する処理を行う
-            msg += file
-        sounds_info_rename.value = msg
-        sounds_info_rename.update()
-    #pick_directory_dialog = ft.FilePicker(
-    #    on_result=pick_directory_result,
-    #    pick_directories=True
-    #)
-    #selected_directory = ft.Text()
+    # ディレクトリの設定関連
     info_sound_dir = ft.Text(size=10)
-    select_sounds_dir= ft.Row([
+    btn_sounds_dir = ft.Row([
                                 ft.ElevatedButton(
                                     "音声フォルダ選択",
-                                    icon=ft.icons.SNIPPET_FOLDER,
+                                    icon=ft.icons.FOLDER,
                                     on_click=lambda _: dialogue_sounds_dir.get_directory_path(),
                                 ),
                                 info_sound_dir,
                         ])
+
     output_dir_path = ft.Text(size=10)
-    select_output_dir= ft.Row([
+    btn_output_dir = ft.Row([
                                 ft.ElevatedButton(
                                     "出力フォルダ選択",
                                     icon=ft.icons.FOLDER,
@@ -315,20 +271,33 @@ def main(page: ft.Page):
                                 ),
                                 output_dir_path,
                         ])
-    default_sound_info = f""
-    sounds_info_in_directory = ft.TextField(
+
+    # リネーム関連
+    status_rename = ft.Text(size=10)
+    btn_extract = ft.Row([
+                            ft.ElevatedButton(
+                                "リネーム実行",
+                                icon=ft.icons.EMOJI_EMOTIONS,
+                                on_click=lambda _: dialogue_output_dir.open_confirm_to_rename(),
+                            ),
+                            status_rename,
+    ])
+
+    sound_info = ft.TextField(
         text_size=10,
-        label="オーディオファイル",
+        label="オーディオファイル:",
         multiline=True,
         min_lines=4,
         read_only=True,
         max_lines=None,
-        value=default_sound_info,
+        value=""
     )
+
     dialogue_sounds_dir = ft.FilePicker(on_result=update_sounds_list_and_rename_list)
     dialogue_output_dir = ft.FilePicker(on_result=get_output_directory)
 
-    sounds_info_rename = ft.TextField(
+    renamed_info = ft.TextField(
+        text_size=10,
         label="ファイル名変更後：",
         multiline=True,
         min_lines=4,
@@ -336,10 +305,12 @@ def main(page: ft.Page):
         max_lines=None,
         value="",
     )
+
     page.overlay.append(dialogue_sounds_dir)
     page.overlay.append(dialogue_output_dir)
 
     name_site = ft.Text()
+    name_ICR = ft.Text()
 
     info_selected_site = ft.TextField(
         label = f"録音サイト:{selected_site}",
@@ -350,6 +321,9 @@ def main(page: ft.Page):
         label = f"ICレコーダ:{selected_ICR}",
         text_size=14,
         disabled= True)
+
+    # dict_ICRから動的に生成する。
+    dropdown_options = [ft.dropdown.Option(key) for key, value in dict_ICR.items()]
 
     t = ft.Tabs(
         selected_index=2,
@@ -362,22 +336,12 @@ def main(page: ft.Page):
                     padding=20,
                     content=ft.Column([
                         ft.Text(value="録音地を登録してください（暫定の入力です）。"),
-                        ft.TextField(label="サイト名(例FukuiEchizen)", value=dict_site['サイト名'], on_change=lambda e: update_dict_site('サイト名', e.control.value)),
-
-                        #ft.TextField(label="県名", value=dict_site['県名'], on_change=lambda e: update_dict_site('県名', e.control.value)),
-                        #ft.TextField(label="市町村", value=dict_site['市町村'], on_change=lambda e: update_dict_site('市町村', e.control.value)),
-                        #ft.TextField(label="緯度", value=dict_site['緯度'], on_change=lambda e: update_dict_site('緯度', e.control.value)),
-                        #ft.TextField(label="経度", value=dict_site['経度'], on_change=lambda e: update_dict_site('経度', e.control.value)),
-                        ft.Row(
-                            [
-                                ft.ElevatedButton(
-                                    text="設定",
-                                    on_click = set_name_site
+                        ft.TextField(label="サイト名(例FukuiEchizen)",
+                                     autofocus = True,
+                                    value=dict_site['サイト名'],
+                                    on_change=lambda e: update_dict_site('サイト名', e.control.value)
                                     ),
-                                ft.Text("ファイル名に用いられるサイト名："),
-                                name_site
-                                ]
-                            ),
+                        name_site
                     ]),
                 ),
             ),
@@ -386,16 +350,20 @@ def main(page: ft.Page):
                 icon=ft.icons.KEYBOARD_VOICE,
                 content=ft.Container(
                     padding=20,
-                    content=ft.Dropdown(
-                        label = "IC Recorder",
-                        hint_text="録音したICレコーダを選択してください。用いるデータはタイムスタンプが開始(START)か終了(STOP)です。",
-                        options=[
-                            ft.dropdown.Option('DM-750: ' + dict_ICR['DM-750']),
-                            ft.dropdown.Option('LS-7: ' + dict_ICR['LS-7']),
-                            ft.dropdown.Option('dummy: ' + dict_ICR['dummy_stop']),
-                        ],
-                        on_change=on_dropdown_change
-                    )
+                    content=ft.Column([
+                        ft.Dropdown(
+                            label = "IC Recorder",
+                            hint_text="録音したICレコーダを選択してください。用いるデータはタイムスタンプが開始(START)か終了(STOP)です。",
+                            options=dropdown_options,
+                            #[
+                            #    ft.dropdown.Option('DM-750' + dict_ICR['DM-750']),
+                            #    ft.dropdown.Option('LS-7' + dict_ICR['LS-7']),
+                            #    ft.dropdown.Option('dummy_stop' + dict_ICR['dummy_stop']),
+                            #],
+                            on_change=on_dropdown_change
+                        ),
+                        name_ICR
+                    ])
                 ),
             ),
             ft.Tab(
@@ -413,10 +381,12 @@ def main(page: ft.Page):
                             info_selected_site,
                             info_selected_ICR,
                         ]),
-                        select_sounds_dir,
-                        select_output_dir,
-                        sounds_info_in_directory,
-                        sounds_info_rename 
+                        btn_sounds_dir,
+                        btn_output_dir,
+                        sound_info,
+                        renamed_info,
+                        btn_extract
+
                     ])
                 ),
             ),
